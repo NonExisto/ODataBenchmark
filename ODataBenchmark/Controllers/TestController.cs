3859f401fa73e32d57d766db41d16af6cbfc034a
Dmitry Mokhnatov <Dmitry.Mokhnatov@syncretis.com>
2021-03-12 07:49:14 +0300

    adding few data endpoint for testing

--- ODataBenchmark/Controllers/TestController.cs
+++ ODataBenchmark/Controllers/TestController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ODataBenchmark.DataModel;
 using System.Linq;
 
@@ -16,10 +17,41 @@ namespace ODataBenchmark.Controllers
 		}
 
 		[HttpGet]
-		public IActionResult SampleData()
+		public IActionResult StartTest()
 		{
 			_benchmarkContext.Database.EnsureCreated();
 			return Ok(_benchmarkContext.Employees.Count());
+		}
+
+		[HttpGet]
+		public IActionResult AllCustomers()
+		{
+			return Ok(_benchmarkContext.Customers.Include(c => c.Owns).Select(c => new
+			{
+				c.BirthDate,
+				c.FirstName,
+				c.LastName,
+				c.Id,
+				c.Login,
+				c.PasswordHash,
+				Owns = c.Owns.Select(o => new { o.Id, o.Name, o.SuperviserId })
+			}));
+		}
+
+		[HttpGet]
+		public IActionResult AllProjects()
+		{
+			return Ok(_benchmarkContext.Projects.Include(c => c.Members).Include(c => c.Owners).Include(c => c.Scopes).Include(c => c.Superviser).ThenInclude(s => s.JobTitles).
+				Select(p => new
+				{
+					p.Id,
+					p.Name,
+					p.SuperviserId,
+					Members = p.Members.Select(m => new { m.Id, m.FirstName, m.LastName, m.PhoneNumber }),
+					Owners = p.Owners.Select(o => new { o.Id, o.FirstName, o.LastName, o.Login, o.PasswordHash }),
+					Scopes = p.Scopes.Select(s => new { s.Id, s.Name, s.Description }),
+					Superviser = new { p.Superviser.Id, p.Superviser.FirstName, p.Superviser.LastName, p.Superviser.PhoneNumber, JobTitles = p.Superviser.JobTitles.Select(j => new { j.Id, j.Name }) }
+				}));
 		}
 	}
 }
